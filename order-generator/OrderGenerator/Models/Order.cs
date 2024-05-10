e10b3345a75fe8ed1e2e0a79c81c0eb54b72b5b7
Mike McDermott <mmcdermott@microsoft.com>
2024-05-10 17:29:41 -0500

    Stubbed out order generator

---
+++ order-generator/OrderGenerator/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace OrderGenerator.Models
+{
+    public class Order
+    {
+        [JsonProperty("id")]
+        public required string Id { get; set; }
+
+        [JsonProperty("items")]
+        public required List<Item> Items { get; set; }
+    }
+}
---
+++ py-eh-consumer/models/Item.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id: int
+    order_id: int
+    description: str
+    price: float
+    
+    @classmethod
+    def _convert_to_real_type_(cls, data):
+        data_type = data.get("type")
+
+        if data_type is None:
+            raise ValueError("Missing 'type' in Item")
+
+        sub = cls._subtypes_.get(data_type)
+
+        if sub is None:
+            raise TypeError(f"Unsupport sub-type: {data_type}")
+
+        return sub(**data)
+    
+    @classmethod
+    def parse_obj(cls, obj):
+        return cls._convert_to_real_type_(obj)
